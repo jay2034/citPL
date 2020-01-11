2146649bc6fd95da6f5172c6d6b3e29e7eddba1e
jay2034 <57559104+jay2034@users.noreply.github.com>
2020-01-11 19:59:45 +0900

    200111

---
+++ 200111/data.py
@@ -0,0 +1,15 @@
+#print():괄호안에 원하는 데이터를 넣을 수 있다.
+#program에서의 메모는 comment 이고 #뒤에 적을 수 있다
+
+#숫자
+print(1)
+print(-2)
+print(3.14)
+print(-8.92)
+#문자
+print('see you again')
+print("mom's phone")
+print('mom\'s phone')
+#boolean
+print(True)
+print(False)
---
+++ 200111/operation.py
@@ -0,0 +1,24 @@
+#산술연산
+print(1+1)
+print(876-1900)
+print(3*4)
+print(9/3)
+print(10/7)
+print(8%3)
+print(3**2)
+print(7%5)
+print(10//3)
+
+#계산의 우선순위 괄호안의 숫자를 먼저 계산
+print(2+2*2)
+print((2+2)*2)
+print('cheese'+'burger')
+#비교연산
+print(5>3)
+print(5<3)
+print(3==3)
+print(5!=7)
+print(8>5)
+print(9<11)
+print(10>=9)
+print(10<=10)
