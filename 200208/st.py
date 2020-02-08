5e500a1583449fcb048bb126da976a3b9e20f2e5
jay2034 <57559104+jay2034@users.noreply.github.com>
2020-02-08 20:06:45 +0900

    200208

---
+++ 200208/st.py
@@ -0,0 +1,7 @@
+nation=["Korea","Japan","USA","France","Russia"]
+Korea={"continent": "Asia", "Salad": "Kimchi", "population": 50000000,"seperated": True }
+
+print(Korea["Salad"])
+print(nation[2])
+print(nation[1])
+print(Korea["population"])
---
+++ 200208/variable.py
@@ -0,0 +1,11 @@
+chicken = 3
+teacher=0.5
+print(chicken-teacher)
+a=3
+print(a)
+a=4
+print(a)
+a=5
+print(a)
+a=a+3
+print(a)
